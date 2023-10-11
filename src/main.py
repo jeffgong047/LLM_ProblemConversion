fed945ef33a68c24d26b53dbcdaceb840ee1ed18
Haoming Gong <hg343@wiselab.cs.rutgers.edu>
2023-10-10 23:52:44 -0400

    initial_testing

--- src/main.py
+++ src/main.py
@@ -4,18 +4,87 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.llms import OpenAI
 from langchain.chains import VectorDBQA
 from langchain.document_loaders import TextLoader
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.chains import VectorDBQA
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import DirectoryLoader
+from langchain.document_loaders import PyPDFLoader
+import chromadb
+from langchain.embeddings import OpenAIEmbeddings
+from chromadb.utils import embedding_functions
 import os
 
-loader = TextLoader('./introductions/Thermodynamic_Limits_of_Electronic_Systems.txt')
-documents = loader.load()
+#Initialize database
+chroma_client = chromadb.Client()
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-texts = text_splitter.split_documents(documents)
+try:
+    chroma_client.delete_collection(name="test")
+except:
+    pass
 
-os.environ["OPENAI_API_KEY"] = "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"
-embeddings = OpenAIEmbeddings()
-vectordb = Chroma.from_documents(texts, embeddings)
+try:
+    chroma_client.delete_collection(name="OpenAI")
+except:
+    pass
 
-qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=vectordb)
-query1 = "Tell me about George Washington"
-qa.run(query1)
+collection = chroma_client.create_collection(name="test")
+
+
+testTextAll = []
+ids = []
+counterS = 0
+solution_path = "/common/home/hg343/Research/LLM_ProblemConversion/datasets/problems/ajp_articles_introductions_Problem"
+problem_path = "/common/home/hg343/Research/LLM_ProblemConversion/datasets/solution/ajp_articles_introductions_Solution"
+
+
+#load chroma database with solutions for physics papers
+for filename in os.listdir(solution_path):
+    if filename.endswith('.txt'):
+        file_path = os.path.join(solution_path, filename)
+        with open(file_path, 'r') as file:
+            text = file.read()
+            testTextAll.append(text)
+            ids.append(filename)
+            counterS = counterS + 1
+
+collection.add(
+    documents=testTextAll,
+    ids=ids
+)
+print('total number of solutions: ',counterS)
+breakpoint()
+#Test
+problemTextAll = []
+idp = []
+counterP = 0
+for filename in os.listdir(problem_path):
+    if filename.endswith('.txt'):
+        file_path = os.path.join(solution_path, filename)
+        with open(file_path, 'r') as file:
+            text = file.read()
+            problemTextAll.append(text)
+            idp.append(filename)
+            counterP +=1
+
+
+results = collection.query(
+    query_texts=problemTextAll,
+    n_results=3
+)
+print('total number of problems: ',counterP)
+#Evaluate with hit-ratio top 3
+results_ids = results['ids']
+results_distance = results['distances']
+results_documents = results['documents']
+correct_num = 0
+for index, p in enumerate(idp):
+    if any(p.lower() == s.lower() for s in results_ids[index]):
+        correct_num+=1
+assert counterP == counterS
+print('The top three hit ratio is: ',correct_num/counterP)
+
+
+
